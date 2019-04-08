a56d5fbfe04054448930a9d0ccc6a69b183ddc1f
Corey Farrell <git@cfware.com>
2019-04-08 11:30:51 -0400

    fix: Drop gulp-filter and gulp-clone dependencies.
    For this module they are easily replaced by simple Transform streams,
    this avoids some out of date dependencies.

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 'use strict';
 
-const pipeline = require('stream.pipeline-shim');
+const {Transform} = require('stream');
 
+const pipeline = require('stream.pipeline-shim');
 const branch = require('branch-pipe');
 const brotli = require('gulp-brotli');
-const clone = require('gulp-clone');
-const filter = require('gulp-filter');
 const gzip = require('gulp-gzip');
+
+function clone() {
+	return new Transform({
+		objectMode: true,
+		transform(file, enc, cb) {
+			cb(null, file.clone());
+		}
+	});
+}
+
+function filter(ext) {
+	return new Transform({
+		objectMode: true,
+		transform(file, enc, cb) {
+			const args = [null];
+			if (file.extname === ext) {
+				args.push(file);
+			}
+
+			cb(...args);
+		}
+	});
+}
 
 function compressFn(ext, compressor) {
 	return (src, options) => pipeline(
@@ -19,11 +41,11 @@ function compressFn(ext, compressor) {
 }
 
 const handleTypes = {
-	br: compressFn('**/*.br', ({skipLarger, brotliOptions}) => {
+	br: compressFn('.br', ({skipLarger, brotliOptions}) => {
 		const options = Object.assign({skipLarger}, brotliOptions, {extension: 'br'});
 		return brotli.compress(options);
 	}),
-	gz: compressFn('**/*.gz', ({skipLarger, gzipOptions}) => {
+	gz: compressFn('.gz', ({skipLarger, gzipOptions}) => {
 		return gzip({skipGrowingFiles: skipLarger, gzipOptions});
 	})
 };
@@ -38,7 +60,7 @@ const defaultOptions = {
 function gulpWebCompress(options = {}) {
 	options = Object.assign({}, defaultOptions, options);
 	if (options.types.length === 0) {
-		return filter('!**');
+		return filter();
 	}
 
 	if (options.types.length > new Set(options.types).size) {
